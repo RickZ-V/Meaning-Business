ab10cd02928f13acbafce900d75a9c2784e03f7c
RickZ-V <77706482+RickZ-V@users.noreply.github.com>
2022-11-21 17:24:07 -0500

    Update Diego Sullcaray.sql

--- SQL queries/Diego Sullcaray.sql
+++ SQL queries/Diego Sullcaray.sql
@@ -11,3 +11,9 @@ join Empresa E2 on E.id = E2.Empresario_id
 join Oferta_Laboral OL on E2.id = OL.Empresa_id
 group by E.nombre ,E2.nombre
 order by count(OL.id)
+
+/*Listar los id de los contratos conjuntamente con el tipo de pago que sea realiza en efectivo*/
+select C.Id , Tdp.nombre
+from Contrato C
+join Tipo_de_pago Tdp on Tdp.id = C.Tipo_de_pago_id
+where Tdp.nombre =  'Efectivo'
