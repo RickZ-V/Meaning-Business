72436c0e5ae6209eac558af819c1c8224a924855
RickZ-V <77706482+RickZ-V@users.noreply.github.com>
2022-11-21 17:24:56 -0500

    Update Lino Abraham.sql

--- SQL queries/Lino Abraham.sql
+++ SQL queries/Lino Abraham.sql
@@ -6,3 +6,8 @@ where sueldo < 1500
 /*Lista de contadores con edad mayor de 35 años.*/
 select nombre, apellido, fecha_nac, telefono from Contador
 select*from contador where fecha_nac <=v 1987 
+
+/*se muestra lista de contadores con precios menores a 1500.*/
+select nombre, apellido, correo from Contador   
+join Oferta_Laboral on Empresa_id 
+where sueldo < 1500
